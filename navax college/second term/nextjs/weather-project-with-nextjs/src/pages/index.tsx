56e35f2718874a9fae0d848da42d4077615497d1
weather <ariafardhossein@gamil.com>
2024-06-13 21:03:56 +0430

    implement weather component

--- navax college/second term/nextjs/weather-project-with-nextjs/src/pages/index.tsx
+++ navax college/second term/nextjs/weather-project-with-nextjs/src/pages/index.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import WeatherInfo from "@/components/WeatherInfo";
 import Weather from "@/components/Weather";
 
 const inter = Inter({ subsets: ["latin"] });
